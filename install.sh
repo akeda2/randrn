075678fea1be82e27b2ea95ceba25b6cdec2fac0
akeda <akeda2@gmail.com>
2022-12-12 14:39:14 +0100

    installer fixed

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 INSTALLDIR="/usr/local/bin/"
 
-[ -f randrn.py ] && sudo cp --remove-destination randrn "$INSTALLDIR" || echo "randrn.py not found! Where are we running this from?"
+[ -f randrn.py ] && sudo cp --remove-destination randrn.py "$INSTALLDIR"/randrn || echo "randrn.py not found! Where are we running this from?"
